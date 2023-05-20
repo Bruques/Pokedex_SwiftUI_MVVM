f6a8eea162d6ef660d13e08dae2728e92470bdf7
Bruno Marques <bruno.marques@townsq.com.br>
2023-05-20 14:09:27 -0300

    fetch pokemon list on the viewModel

--- Pokedex_SwiftUI_MVVM/PokemonListViewModel.swift
+++ Pokedex_SwiftUI_MVVM/PokemonListViewModel.swift
@@ -9,12 +9,20 @@ import Foundation
 
 class PokemonListViewModel: ObservableObject {
     var service = Service()
+    @Published var pokemons = [PokemonEntry]()
     
     init() {
-        self.fetchPokemonsList()
+        fetchPokemonsList()
     }
     
     private func fetchPokemonsList() {
-        
+        service.fetchPokemonList { response in
+            switch response {
+            case .success(let pokemons):
+                self.pokemons = pokemons
+            case.failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 }

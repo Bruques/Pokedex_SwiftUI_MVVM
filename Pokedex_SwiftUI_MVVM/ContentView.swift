8b05e64654b4ed6834d087935885cb79b52c0d81
Bruno Marques <bruno.marques@townsq.com.br>
2023-05-20 13:52:16 -0300

    First commit, base project

---
+++ Pokedex_SwiftUI_MVVM/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pokedex_SwiftUI_MVVM
+//
+//  Created by Bruno Marques on 20/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pokedex_SwiftUI_MVVM/Pokedex_SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_SwiftUI_MVVMApp.swift
+//  Pokedex_SwiftUI_MVVM
+//
+//  Created by Bruno Marques on 20/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

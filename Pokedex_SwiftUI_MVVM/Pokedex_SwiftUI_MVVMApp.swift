560b04fa42348a0786cd8fbddd84a59139526169
Bruno Marques <bruno.marques@townsq.com.br>
2023-05-20 13:57:35 -0300

    create view model

--- Pokedex_SwiftUI_MVVM/Pokedex_SwiftUI_MVVMApp.swift
+++ Pokedex_SwiftUI_MVVM/Pokedex_SwiftUI_MVVMApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Pokedex_SwiftUI_MVVMApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PokemonListView(viewModel: PokemonListViewModel())
         }
     }
 }
---
+++ Pokedex_SwiftUI_MVVM/PokemonListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonListViewModel.swift
+//  Pokedex_SwiftUI_MVVM
+//
+//  Created by Bruno Marques on 20/05/23.
+//
+
+import Foundation
+
+class PokemonListViewModel: ObservableObject {
+    
+    
+}
